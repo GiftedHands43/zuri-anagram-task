13b0b2bc534e4c2ad8db20242705b15894d098b1
GiftedHands43 <100702359+GiftedHands43@users.noreply.github.com>
2022-05-28 18:41:40 +0100

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,29 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+word="elbow"
+anagram="below"
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    word = word.lower()
+    anagram = anagram.lower()
+    
+if (len(word) == len(anagram)):
+    
+    sorted_word = sorted(word)
+    sorted_anagram = sorted(anagram)
+    
+    if(sorted_word == sorted_anagram):
+    	print("True")
+    else:
+    		print("False")
+    		
+else:
+    print("False")
+    
+word = "Dear"
+anagram = "Read"
+ https://github.com/GiftedHands43/zur-readi-text-file-task.git
+find_anagram(word, anagram)
